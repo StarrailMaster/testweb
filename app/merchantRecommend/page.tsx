9dd6e00392fc788eb5c0d7390a26a2e15328202d
StarrailMaster <347392867@qq.com>
2024-10-30 21:14:55 +1100

    Update page.tsx

--- app/merchantRecommend/page.tsx
+++ app/merchantRecommend/page.tsx
@@ -21,7 +21,7 @@ const MerchantRecommend = () => {
     setInput('');
 
     try {
-      const response = await axios.post('http://localhost:8080/api/merchant_recommend/generateRecommend', {
+      const response = await axios.post('http://13.55.190.75:8080/api/merchant_recommend/generateRecommend', {
         searchText: input
       });
       const botReply = { sender: 'system', text: response.data.recommendContent };
